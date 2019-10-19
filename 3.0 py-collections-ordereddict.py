645f6a92c397b7669c836ab8acf85a80c6b29936
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-10-19 12:03:19 -0500

    Collections OrderedDict improved

--- 3.0 py-collections-ordereddict.py
+++ 3.0 py-collections-ordereddict.py
@@ -3,15 +3,8 @@ from collections import OrderedDict
 if __name__ == '__main__':
     ordered_dict = OrderedDict()
     for i in range(int(input())):
-        pair = input()
-        key = pair[:pair.rfind(' ') + 1].strip()
-        value = pair[pair.rfind(' '):]
-        if key in ordered_dict:
-            ordered_dict[key].append(int(value))
-        else:
-            ordered_dict[key] = [int(value)]
+        key, value = input().rsplit(' ', 1)
+        ordered_dict[key] = ordered_dict.get(key, 0) + int(value)
 
     for key, value in ordered_dict.items():
-        print(key, sum(value))
-        # print(key.strip() + ': Quantity bought:', str(len(value)) + ', Price:', value[0])
-        # print('Net Price:', sum(value))
+        print(key, value)
